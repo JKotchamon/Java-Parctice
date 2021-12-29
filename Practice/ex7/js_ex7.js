2aacd44b239496974b6bf0ab994d138e29ddc92f
Kotchamon <kotchamon.jk@gmail.com>
2021-12-29 07:54:15 +0700

    update Ex8 from local to remote

---
+++ Practice/ex7/js_ex7.js
@@ -0,0 +1,12 @@
+//<!--Ex:7 Javascript String concatenation-->
+
+var text1 = "I'm studing ";
+var text2 = "JavaScript Programing";
+var text3 = "Visual Studio Code";
+document.getElementById("ex7").innerHTML = text1+text2+" with "+text3;
+text1 += "Python Programing"; //text1 = text + "..."
+document.write(text1);
+let text4 ="31" + " Mon " + "4";
+let text5 = "October" + 2021;
+document.write("<br><br>"+text4+"  "+text5);
+
